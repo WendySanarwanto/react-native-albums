e5023a180b8ebd3e53e0744cf77d23faa505207f
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-04-28 09:42:04 +0800

    Display title , artist name on AlbumDetail's header

--- components/AlbumDetail.js
+++ components/AlbumDetail.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
-  const { title } = props.album;
+  const { title, artist } = props.album;
   return (
     <Card>
       <CardSection>
-        <Text>{title}</Text>
+        <View></View>
+        <View style={styles.headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
 };
 
 export default AlbumDetail;
