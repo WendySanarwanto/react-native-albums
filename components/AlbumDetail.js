1ec4e1078a94ff21b98a99ac281b451ac4240f04
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-04-28 09:52:47 +0800

    Added Image thumbnail on Album Detail component.

--- components/AlbumDetail.js
+++ components/AlbumDetail.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-  const { title, artist } = props.album;
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { headerContentStyle, thumbnailStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <View></View>
-        <View style={styles.headerContentStyle}>
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
           <Text>{title}</Text>
           <Text>{artist}</Text>
         </View>
@@ -22,6 +26,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 };
 
